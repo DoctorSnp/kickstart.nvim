3ad1c33b0615d4002775c578437093f89d360541
Davydov Denis <davydovdv@sinara-group.com>
2024-07-28 22:37:33 +0000

    Slitting configuration

---
+++ lua/my_conf/my_config.lua
@@ -0,0 +1,20 @@
+require 'my_conf.settings'
+require 'my_conf.keymaps'
+
+require 'kickstart.plugins.files_tree'
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+require 'my_conf.lazy_module'
+require 'my_conf.nvim-tree-settings'
---
+++ lua/my_settings.lua
@@ -0,0 +1,1 @@
+require 'lua.kickstart.plugins.monokai'
